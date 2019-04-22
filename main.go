11632bf66625fa5637b5a5e77f8714eb21c31833
Rony Rabak <ronyrabak@gmail.com>
2019-04-22 10:55:41 +0100

    router test

---
+++ main.go
@@ -0,0 +1,15 @@
+package router
+
+import "net/http"
+
+// Route is a Routing object
+type Route struct {
+	Route   string `json:"route"`
+	Handler func(http.ResponseWriter, *http.Request)
+}
+
+// Router is a Router object
+type Router struct {
+	Routes   []Route
+	Fallback Route
+}
